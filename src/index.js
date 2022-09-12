4cbf07140e507424dd9088810f4f7dc9f32dcc73
gnos28 <gnos28@gmail.com>
2022-09-12 18:07:22 +0200

    init typeorm

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const typeorm = require("typeorm");
+const Wilder = require("./Wilder");
+
+const app = express();
+
+const dataSource = new typeorm.DataSource({
+  type: "sqlite",
+  database: "./wildersdb.sqlite",
+  synchronize: true,
+  entities: [require("./Wilder")],
+});
+
+app.get("/", async (req, res) => {
+  const listWilders = await dataSource.getRepository(Wilder).find();
+  console.log("listWilders", listWilders);
+  res.send(listWilders);
+});
+
+//Start Server
+const start = async () => {
+  await dataSource.initialize();
+  dataSource.getRepository(Wilder).save({ name: "Julien VIGNERON" });
+
+  app.listen(3000, () => console.log("OK"));
+};
+
+start();
