4b4aebb21abaf5b5f75e49491e77ef64c45947a4
gnos28 <gnos28@gmail.com>
2022-09-16 07:36:47 +0200

    bugfix when editing description

--- frontend/services/convert.js
+++ frontend/services/convert.js
@@ -1,9 +1,11 @@
 const convertLineBreakToBr = (description) =>
-  description.split("\n").map((par, parIndex) => (
-    <span key={parIndex}>
-      {par}
-      <br />
-    </span>
-  ));
+  description
+    ? description.split("\n").map((par, parIndex) => (
+        <span key={parIndex}>
+          {par}
+          <br />
+        </span>
+      ))
+    : "";
 
 export { convertLineBreakToBr };
